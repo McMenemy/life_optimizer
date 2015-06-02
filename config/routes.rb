bbe9fc817a3d6f8d6bcfda3023edae1ef114d804
jabberwick <mr_mcmenemy@yahoo.com>
2015-06-02 23:28:11 +0000

    added habit and data panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root                  'static_pages#home'
   get    'dashboard'    => 'static_pages#dashboard'
+  get    'habit_panel'  => 'static_pages#habit_panel'
+  get    'data_panel'   => 'static_pages#data_panel'
   get    'help'         => 'static_pages#help'
   get    'about'        => 'static_pages#about'
   get    'contact'      => 'static_pages#contact'
