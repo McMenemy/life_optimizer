bf0123e5275fb18d36ade18ddd8d73f914ada408
jabberwick <mr_mcmenemy@yahoo.com>
2015-07-17 20:50:44 +0000

    habit creation now works

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -3,8 +3,5 @@ class Habit < ActiveRecord::Base
   validates :user_id, presence: true
   validates :title, presence: true
   validates :classification, presence: true
-  validates :completed, presence: true
-  validates :time_stamp, presence: true
-  validates :quantifier, presence: true
   validates :quantifier_unit, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :habits,              only: [:create, :destroy]
+  resources :habits,              only: [:new, :create, :destroy]
 end
